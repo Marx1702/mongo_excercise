85deb590728679f3df7bb6f44681a2286c3b176c
Marcelo Arteaga <marce.arteaga1997@gmail.com>
2024-11-12 19:15:36 -0300

    Excercise

---
+++ pruebamongo.js
@@ -0,0 +1,24 @@
+use baseclases;
+
+db.alumnos.insertMany([
+    {
+        nombre:"Juan",
+        apellido:"Arteaga",
+        edad: 20,
+    },
+    {
+        nombre:"Pedro",
+        apellido:"Perez",
+        edad: 25,
+    },
+    {
+        nombre:"Emiliano",
+        apellido:"Estevez",
+        edad:23
+    },
+    {
+        nombre:"Maximiliano",
+        apellido:"Larrain",
+        edad: 30
+    }
+])
